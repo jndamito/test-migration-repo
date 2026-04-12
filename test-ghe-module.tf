a1b6ec9aee60ec43abbaafc24f719f5d766f06a7
Josias NDAM <134744341+jndamito@users.noreply.github.com>
2026-04-12 00:40:18 -0400

    chore: migrate URLs (ghe → to → ado)

--- test-ghe-module.tf
+++ test-ghe-module.tf
@@ -1,12 +1,12 @@
 module "s3_bucket" {
-  source = "git::https://aviva-canada.ghe.com/its-cld-plat-eng/aws-s3.git?ref=v1.2.0"
+  source = "git::https://aviva-canada@dev.azure.com/aviva-canada/CloudPlatform/_git/aws-s3?ref=v1.2.0"
   bucket_name = "my-bucket"
 }
 
 module "vpc" {
-  source = "git::https://aviva-canada.ghe.com/its-cld-plat-eng/aws-vpc.git?ref=v2.0.0"
+  source = "git::https://aviva-canada@dev.azure.com/aviva-canada/CloudPlatform/_git/aws-vpc?ref=v2.0.0"
 }
 
 module "iam" {
-  source = "git::https://aviva-canada.ghe.com/its-cld-plat-eng/aws-iam.git?ref=v3.1.0"
+  source = "git::https://aviva-canada@dev.azure.com/aviva-canada/CloudPlatform/_git/aws-iam?ref=v3.1.0"
 }
