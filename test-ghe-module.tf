11be3d891f427c91b8fd7911d4c6296f38f8399c
Josias NDAM <134744341+jndamito@users.noreply.github.com>
2026-04-12 00:44:15 -0400

    chore: migrate URLs in tag v1.0.0 (ado → to → ghe)

--- test-ghe-module.tf
+++ test-ghe-module.tf
@@ -1,4 +1,4 @@
 module "s3_bucket" {
-  source = "git::https://aviva-canada@dev.azure.com/aviva-canada/CloudPlatform/_git/aws-s3?ref=v1.0.0"
+  source = "git::https://aviva-canada.ghe.com/its-cld-plat-eng/aws-s3?ref=v1.0.0"
   bucket_name = "my-bucket"
 }
