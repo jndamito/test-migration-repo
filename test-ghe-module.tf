9bb006d3a9dee879dd8b573ee4658d7afcc175e9
jndamito <josiasndam35@gmail.com>
2026-04-11 23:59:55 -0400

    test: add module with GHE URL v1.1.0

--- test-ghe-module.tf
+++ test-ghe-module.tf
@@ -1,4 +1,8 @@
 module "s3_bucket" {
-  source = "git::https://aviva-canada.ghe.com/its-cld-plat-eng/aws-s3.git?ref=v1.0.0"
+  source = "git::https://aviva-canada.ghe.com/its-cld-plat-eng/aws-s3.git?ref=v1.1.0"
   bucket_name = "my-bucket"
+}
+
+module "vpc" {
+  source = "git::https://aviva-canada.ghe.com/its-cld-plat-eng/aws-vpc.git?ref=v2.0.0"
 }
